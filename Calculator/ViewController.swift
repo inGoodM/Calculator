19bc8a6a992363b577c0de603e6d40fef2a8cfec
Kholodovskyi Artem Igorevich <31437162+inGoodM@users.noreply.github.com>
2022-05-24 23:25:49 +0300

    first final visual look

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,19 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numbers: String = ""
+    var result: Float = 0.0
+    
     @IBOutlet weak var resultLable: UILabel!
     @IBOutlet weak var realTimeTipping: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+        resultLable.text = ""
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
+        numbers = sender.titleLabel?.text! ?? ""
+        
+        switch numbers {
+        case "0": makeConcatetion(numbers)
+        case "1": makeConcatetion(numbers)
+        case "2": makeConcatetion(numbers)
+        case "3": makeConcatetion(numbers)
+        case "4": makeConcatetion(numbers)
+        case "5": makeConcatetion(numbers)
+        case "6": makeConcatetion(numbers)
+        case "7": makeConcatetion(numbers)
+        case "8": makeConcatetion(numbers)
+        case "9": makeConcatetion(numbers)
+        case ".": makeConcatetion(numbers)
+        case "AC": clear()
+        case "+/-": changePolar()
+        case "%": findPercent()
+        case "÷": division()
+        case "×": multy()
+        case "-": minusing()
+        case "+": plussing()
+        case "=": equalling()
+        default: break
+        }
         
     }
     
-    
+    func makeConcatetion(_ senderText: String) {
+        realTimeTipping.text! += senderText
+    }
+    func clear() {
+        
+    }
+    func changePolar () {
+        
+    }
+    func findPercent() {
+        
+    }
+    func division () {
+        
+    }
+    func multy() {
+        
+    }
+    func minusing () {
+        
+    }
+    func plussing () {
+        
+    }
+    func equalling () {
+        
+    }
 }
 
