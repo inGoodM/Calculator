030fc6fc61cf01c0d66890fcb731206d2b3b6798
Kholodovskyi Artem Igorevich <31437162+inGoodM@users.noreply.github.com>
2022-05-24 15:10:02 +0300

    Connect buttons to viewController via outLets

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLable: UILabel!
+    @IBOutlet weak var realTimeTipping: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
 }
 
