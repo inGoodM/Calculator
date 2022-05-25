0e75f7aee60678c3b64b8bae4dee10e93f9f9e72
Kholodovskyi Artem Igorevich <31437162+inGoodM@users.noreply.github.com>
2022-05-26 02:35:53 +0300

    Need fix several moments: - division by zero - displaying result of calculation without ".0" (for example: 4*10 = 40.0, need to display  result as 40) - add function to %button - to equate when sign operands buttons pressed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,72 +8,104 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var numbers: String = ""
-    var result: Float = 0.0
     
-    @IBOutlet weak var resultLable: UILabel!
-    @IBOutlet weak var realTimeTipping: UILabel!
+    //MARK: variable and constant block
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        resultLable.text = ""
-    }
-
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        numbers = sender.titleLabel?.text! ?? ""
-        
-        switch numbers {
-        case "0": makeConcatetion(numbers)
-        case "1": makeConcatetion(numbers)
-        case "2": makeConcatetion(numbers)
-        case "3": makeConcatetion(numbers)
-        case "4": makeConcatetion(numbers)
-        case "5": makeConcatetion(numbers)
-        case "6": makeConcatetion(numbers)
-        case "7": makeConcatetion(numbers)
-        case "8": makeConcatetion(numbers)
-        case "9": makeConcatetion(numbers)
-        case ".": makeConcatetion(numbers)
-        case "AC": clear()
-        case "+/-": changePolar()
-        case "%": findPercent()
-        case "÷": division()
-        case "×": multy()
-        case "-": minusing()
-        case "+": plussing()
-        case "=": equalling()
-        default: break
+    let signButtonsArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var firstOperand: Double = 0.0
+    var secondOperand: Double = 0.0
+    var signOperator = ""
+    var result: Double {
+        get {
+                return Double(displayResult.text!)!
         }
-        
+        set {
+            let str = String(newValue)
+            let valueArray = str.components(separatedBy: ".")
+            if valueArray[1] == "0" {
+                displayResult.text! = "\(valueArray[0])"
+            } else {
+                displayResult.text! = "\(newValue)"
+                isTyping = false
+            }
+        }
+    }
+    var isDotContain = false
+    var isTyping = false
+    
+    @IBOutlet weak var displayResult: UILabel!
+    
+    //MARK: Add sign to display
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let sign = sender.titleLabel?.text!
+        switch sign {
+        case sign where isTyping == false:
+            displayResult.text! = ""
+            displayResult.text! += sign!
+            isTyping = true
+        default:
+            displayResult.text! += sign!
+        }
     }
     
-    func makeConcatetion(_ senderText: String) {
-        realTimeTipping.text! += senderText
+    //MARK: Clear button functionality
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        isTyping = false
+        isDotContain = false
+        displayResult.text! = "0"
+        firstOperand = 0.0
+        secondOperand = 0.0
     }
-    func clear() {
+    
+    //MARK: Operators button functionality
+    
+    @IBAction func operatorButtonPressed(_ sender: UIButton) {
+        firstOperand = Double(displayResult.text!) ?? 0.0
+        signOperator = sender.titleLabel?.text! ?? ""
+        displayResult.text! = ""
+        isDotContain = false
+        isTyping = false
+    }
+    
+    //MARK: Equalating button functionality
+    
+    @IBAction func equalButtonPressed(_ sender: UIButton) {
+        secondOperand = Double(displayResult.text!) ?? 0.0
+        isTyping = false
+        
+        switch signOperator {
+        case "÷" where secondOperand != 0:
+            result = firstOperand / secondOperand
+        case "×":
+            do { result = firstOperand * secondOperand }
+        case "-":
+            do { result = firstOperand - secondOperand }
+        case "+":
+            do { result = firstOperand + secondOperand }
+        default:
+            displayResult.text! = "division by zero"
+        }
+        displayResult.text! = "\(result)"
         
     }
-    func changePolar () {
-        
+    //MARK: Dot button functionality
+    
+    @IBAction func dotButtonPressed(_ sender: UIButton) {
+        if isTyping && !isDotContain {
+            displayResult.text! = displayResult.text! + "."
+        } else if !isTyping && !isDotContain {
+            displayResult.text! = "0."
+        }
     }
-    func findPercent() {
-        
+    
+    //MARK: plus-minus button
+    
+    @IBAction func plusMinusButton(_ sender: UIButton) {
+        result = -result
     }
-    func division () {
-        
-    }
-    func multy() {
-        
-    }
-    func minusing () {
-        
-    }
-    func plussing () {
-        
-    }
-    func equalling () {
-        
-    }
+    
+    
 }
 
